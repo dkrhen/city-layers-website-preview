d2ed00589057862186e07578ca23cca798a5ad57
dkrhen <domagoj.krhen@citylayers.eu>
2026-05-26 11:56:54 +0200

    fix Directus setup, uploads volume, remove token auth

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,6 +1,8 @@
-import { createDirectus, rest, readItems } from '@directus/sdk';
+import { createDirectus, rest, staticToken, readItems } from '@directus/sdk';
 
-const directus = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL).with(rest());
+const directus = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL)
+    .with(staticToken(import.meta.env.DIRECTUS_TOKEN))
+    .with(rest());
 
 export default directus;
 export { readItems };
