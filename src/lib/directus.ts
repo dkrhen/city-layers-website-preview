cb6efe1999fadb4912ccc89725ec6a8446611eb2
dkrhen <domagoj.krhen@citylayers.eu>
2026-05-28 00:30:23 +0200

    Remove source files, keep only dist contents

--- src/lib/directus.ts
+++
@@ -1,6 +0,0 @@
-import { createDirectus, rest, readItems } from '@directus/sdk';
-
-const directus = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL).with(rest());
-
-export default directus;
-export { readItems };
